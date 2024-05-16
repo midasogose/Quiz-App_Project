dc12938ffebbc653efa0ae3489fcf75c0ce20a03
Abdulfetah-Sultan <165145343+Abdulfetah-Sultan@users.noreply.github.com>
2024-05-16 21:18:56 +0300

    js code edited

--- js/qz.js
+++ js/qz.js
@@ -1,73 +1,73 @@
-//Question bank
-var questionBank= [
+let questions = [
     {
-        question : 'Eritrea, which became the 182nd member of the UN in 1993, is in the continent of',
-        option : ['Asia','Africa','Europe','Australia'],
-        answer : 'Africa'
-    },
+    numb: 1,
+    question: "What does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Preprocessor",
+      "Hyper Text Markup Language",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language"
+    ]
+  },
     {
-        question : 'Garampani sanctuary is located at',
-        option : ['Junagarh, Gujarat','Diphu, Assam','Kohima, Nagaland','Gangtok, Sikkim'],
-        answer : 'Diphu, Assam'
-    },
+    numb: 2,
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet"
+    ]
+  },
     {
-        question : 'For which of the following disciplines is Nobel Prize awarded?',
-        option : ['Physics and Chemistry','Physiology or Medicine','Literature, Peace and Economics','All of the above'],
-        answer : 'All of the above'
-    },
+    numb: 3,
+    question: "What does PHP stand for?",
+    answer: "Hypertext Preprocessor",
+    options: [
+      "Hypertext Preprocessor",
+      "Hypertext Programming",
+      "Hypertext Preprogramming",
+      "Hometext Preprocessor"
+    ]
+  },
     {
-        question : 'Hitler party which came into power in 1933 is known as',
-        option : ['Labour Party','Nazi Party','Ku-Klux-Klan','Democratic Party'],
-        answer : 'Nazi Party'
-    },
+    numb: 4,
+    question: "What does SQL stand for?",
+    answer: "Structured Query Language",
+    options: [
+      "Stylish Question Language",
+      "Stylesheet Query Language",
+      "Statement Question Language",
+      "Structured Query Language"
+    ]
+  },
     {
-        question : 'First human heart transplant operation conducted by Dr. Christiaan Barnard on Louis Washkansky, was conducted in',
-        option : ['1967','1968','1958','1922'],
-        answer : '1967'
-    }
-]
+    numb: 5,
+    question: "What does XML stand for?",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXtensible Markup Language",
+      "eXecutable Multiple Language",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language"
+    ]
+  },
+  
 
-var question= document.getElementById('question');
-var quizContainer= document.getElementById('quiz-container');
-var scorecard= document.getElementById('scorecard');
-var option0= document.getElementById('option0');
-var option1= document.getElementById('option1');
-var option2= document.getElementById('option2');
-var option3= document.getElementById('option3');
-var next= document.querySelector('.next');
-var points= document.getElementById('score');
-var span= document.querySelectorAll('span');
-var i=0;
-var score= 0;
-
-//function to display questions
-function displayQuestion(){
-    for(var a=0;a<span.length;a++){
-        span[a].style.background='none';
-    }
-    question.innerHTML= 'Q.'+(i+1)+' '+questionBank[i].question;
-    option0.innerHTML= questionBank[i].option[0];
-    option1.innerHTML= questionBank[i].option[1];
-    option2.innerHTML= questionBank[i].option[2];
-    option3.innerHTML= questionBank[i].option[3];
-    stat.innerHTML= "Question"+' '+(i+1)+' '+'of'+' '+questionBank.length;
-}
-//function to calculate scores
-function calcScore(e){
-    if(e.innerHTML===questionBank[i].answer && score<questionBank.length)
-    {
-        score= score+1;
-        document.getElementById(e.id).style.background= 'limegreen';
-    }
-    else{
-        document.getElementById(e.id).style.background= 'tomato';
-    }
-    setTimeout(nextQuestion,300);
-}
-
-//function to display next question
-function nextQuestion(){
-    if(i<questionBank.length-1)
+     {
+     numb: 6,
+     question: "the option is here",
+      answer: "option 2", 
+     options: [
+        "option 1",
+        "option 2",
+        "option 3",
+        "option 4"
+    ]
+   },
+];
     {
         i=i+1;
         displayQuestion();
@@ -77,7 +77,7 @@ function nextQuestion(){
         quizContainer.style.display= 'none';
         scoreboard.style.display= 'block'
     }
-}
+
 
 //click events to next button
 next.addEventListener('click',nextQuestion);
